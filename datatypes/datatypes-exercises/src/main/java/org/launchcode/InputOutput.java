ccf56ffbea1b38a64df8c08f5ba26227cc86c128
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-03 00:36:37 -0500

    CT completed - created class to input/output name & number

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/InputOutput.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/InputOutput.java
@@ -1,4 +1,17 @@
 package org.launchcode;
 
+import java.util.Scanner;
+
 public class InputOutput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = input.nextLine();
+
+        System.out.println("What is your favorite number?");
+        int numFav = input.nextInt();
+
+        System.out.println(name + "'s favorite number is " + numFav);
+    }
 }
