9b30b24b28d71fceb8c3fab8f391c42905bc4393
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-03 00:52:25 -0500

    CT complete - created class to input miles & gallons consumed and outputs mpg

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/MilesPerGallon.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/MilesPerGallon.java
@@ -1,4 +1,18 @@
 package org.launchcode;
 
+import java.util.Scanner;
+
 public class MilesPerGallon {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many miles driven?");
+        Double numMilesDriven = input.nextDouble();
+
+        System.out.println("How many gallons of gas consumed?");
+        Double numGallonsConsumed = input.nextDouble();
+
+        double mpg = numMilesDriven / numGallonsConsumed;
+        System.out.println("Currently running on " + mpg + " mpg.");
+    }
 }
