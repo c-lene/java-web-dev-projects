0ef8efb9d380d4084fc40f79aa94db719f899a61
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-03 00:46:36 -0500

    CT completed - created class to input length & width and output Area of Rectangle

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/AreaOfRectangle.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/AreaOfRectangle.java
@@ -1,4 +1,18 @@
 package org.launchcode;
 
+import java.util.Scanner;
+
 public class AreaOfRectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the Length of the Rectangle: ");
+        int numLength = input.nextInt();
+
+        System.out.println("Enter the Width of the Rectangle: ");
+        int numWidth = input.nextInt();
+
+        int numAreaRectangle = numLength * numWidth;
+        System.out.println("The Area of the Rectangle is " + numAreaRectangle + ".");
+    }
 }
