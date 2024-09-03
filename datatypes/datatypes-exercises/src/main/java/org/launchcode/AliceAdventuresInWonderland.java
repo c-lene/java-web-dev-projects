ef3ad6dee9c6d5f6af7c993ffe959a266783d018
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-03 03:12:36 -0500

    CT completed part E - modifies sentence to remove User Input search term

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/AliceAdventuresInWonderland.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/AliceAdventuresInWonderland.java
@@ -11,15 +11,51 @@ public class AliceAdventuresInWonderland {
                 "‘and what is the use of a book,’ " +
                 "thought Alice ‘without pictures or conversation?’";
 
+
+        // Prints out the firstSentence
         System.out.println(firstSentence);
 
+
+        // Declare & Initialize 'input' variable
         Scanner input = new Scanner(System.in);
 
+
+        // Ask user to input search term and initialize & declare variable
         System.out.println("What term would you like to search for within the first sentence of 'Alice's Adventures in Wonderland'?");
+
+        // Declare & Initialize 'searchTerm' as a String ITEM
         String searchTerm = input.next();
 
-        if (firstSentence.contains(searchTerm)) {
+
+        // Ensures user input is Case-Insensitive
+        String firstSentenceLowercase = firstSentence.toLowerCase();
+        String searchTermLowercase = searchTerm.toLowerCase();
+
+
+        // Uses If/Else statement to search for 'searchTerm' & prints "true" / "false"
+        if (firstSentenceLowercase.contains(searchTermLowercase)) {
             System.out.println("true");
+
+            // Declares & Initialize the Index of 'searchTerm'
+            int searchTermIndex = firstSentenceLowercase.indexOf(searchTermLowercase);
+
+            // Declares & Initialize the Length of 'searchTerm'
+            int searchTermLength = searchTermLowercase.length();
+
+            // Prints out Index & Length of 'searchTerm'
+            System.out.println("The entered search term initially appears at Index " + searchTermIndex + " and is " + searchTermLength + " characters long!");
+
+
+            // Modifying firstSentence to remove the 'searchTerm'
+            int endIndex = searchTermIndex + searchTermLength;
+            int statementEndIndex = firstSentence.length();
+
+            String modifiedStart = firstSentence.substring(0, searchTermIndex);
+            String modifiedEnd = firstSentence.substring(endIndex, statementEndIndex-1);
+
+            String modifiedStatement = modifiedStart + modifiedEnd;
+            System.out.println(modifiedStatement);
+
         } else {
             System.out.println("false");
         }
