a92ef9498081d2747bb93550a9c717aafd6ec083
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-03 01:41:15 -0500

    CT completed - created class to input search term and output true/false whether term was present

---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/AliceAdventuresInWonderland.java
@@ -0,0 +1,27 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class AliceAdventuresInWonderland {
+    public static void main(String[] args) {
+        String firstSentence =
+                "Alice was beginning to get very tired of sitting by her sister on the bank, " +
+                "and of having nothing to do: once or twice she had peeped into the book her sister was reading, " +
+                "but it had no pictures or conversations in it, " +
+                "‘and what is the use of a book,’ " +
+                "thought Alice ‘without pictures or conversation?’";
+
+        System.out.println(firstSentence);
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What term would you like to search for within the first sentence of 'Alice's Adventures in Wonderland'?");
+        String searchTerm = input.next();
+
+        if (firstSentence.contains(searchTerm)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
