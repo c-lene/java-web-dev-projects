dbe859c3c04f21ae5194ccf9d16ecc8f31dd05c2
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-05 22:02:01 -0500

    src/main/java/org/launchcode/Area.java

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -14,6 +14,7 @@ public class Area {
         double radius;
         double areaOfCircle;
 
+
         // Original Code
 //        System.out.println("Enter a radius:");
 //        double radius = input.nextDouble();
@@ -22,22 +23,41 @@ public class Area {
 //        System.out.println("The Area of a Circle with a Radius of " + radius + " is: " + areaOfCircle);
 
 
-        // Bonus Mission - Validation
-        System.out.println("Please enter a radius:");
+        // Bonus Mission #1 - Validation
+//        System.out.println("Please enter a radius:");
+//
+//        // Checks if input is a NUMBER
+//        if (input.hasNextDouble()) {
+//            radius = input.nextDouble();
+//
+//            // Checks if input is a POSITIVE number
+//            if (radius > 0) {
+//                areaOfCircle = Circle.getArea(radius);
+//                System.out.println("The Area of a Circle with a Radius of " + radius + " is: " + areaOfCircle);
+//            } else {
+//                System.out.println("The entered Radius should be a POSITIVE number and is NOT a valid input!");
+//            }
+//
+//        } else {
+//            System.out.println("The entered Radius is NOT a valid number!");
+//        }
 
-        if (input.hasNextDouble()) {
+
+        // Bonus Mission #2 - Using Loops to Re-prompt
+        do {
+            System.out.println("Please enter a POSITIVE number for the Radius: ");
+
+            // Using While Loop to ensure input is a NUMBER
+            while (!input.hasNextDouble()) {
+                System.out.println("Invalid Input. The Radius muse be a NUMBER! Try again!");
+                input.next();
+            }
             radius = input.nextDouble();
 
-            if (radius > 0) {
-                areaOfCircle = Circle.getArea(radius);
-                System.out.println("The Area of a Circle with a Radius of " + radius + " is: " + areaOfCircle);
-            } else {
-                System.out.println("The entered Radius should be a POSITIVE number and is NOT a valid input!");
-            }
+        } while (radius < 0);
 
-        } else {
-            System.out.println("The entered Radius is NOT a valid input!");
-        }
+        areaOfCircle = Circle.getArea(radius);
+        System.out.println("The Area of a Circle with a Radius of " + radius + " is: " + areaOfCircle);
 
         input.close();
     }
