343f91921f9a71382879d83c6db627eea583fa8d
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-05 20:11:26 -0500

    CT completed - uses input radius to calculate Area of Circle

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,4 +1,17 @@
 package org.launchcode;
 
+import java.util.Scanner;
+
 public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a radius:");
+        double radius = input.nextFloat();
+
+//        double areaOfCircle = (3.14 * radius * radius);
+        double areaOfCircle = Circle.getArea(radius);
+
+        System.out.println("The Area of a Circle of Radius " + radius + " is : " + areaOfCircle);
+    }
 }
