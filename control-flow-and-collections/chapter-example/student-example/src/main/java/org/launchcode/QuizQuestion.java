47f669decb6f642e082f5c3ddc2f32ca4ad904f6
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-06 01:38:15 -0500

    CT completed

--- control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/QuizQuestion.java
+++ control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/QuizQuestion.java
@@ -1,4 +1,22 @@
 package org.launchcode;
 
+import java.util.Scanner;
+
+// Check Your Understanding Question
 public class QuizQuestion {
+    public static void main(String[] args) {
+
+        System.out.println("Are you a Space Cadet? yes or no");
+        Scanner in = new Scanner(System.in);
+        String response = in.next();
+
+        switch (response) {
+            case "yes":
+                System.out.println("Greetings cadet.");
+            case "no":
+                System.out.println("Greetings normie.");
+            default:
+                System.out.println("Are you an alien?");
+        }
+    }
 }
