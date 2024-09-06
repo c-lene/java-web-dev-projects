6e1ca973e416e9b24faeae757135770c2afa1ae0
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-05 21:48:35 -0500

    CT completed - Bonus Mission #1 - Adding validation to ensure input is a positive number

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -3,15 +3,42 @@ package org.launchcode;
 import java.util.Scanner;
 
 public class Area {
+
+    static void numValidation(String radius) {
+
+    }
+
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        double radius;
+        double areaOfCircle;
 
-        System.out.println("Enter a radius:");
-        double radius = input.nextFloat();
+        // Original Code
+//        System.out.println("Enter a radius:");
+//        double radius = input.nextDouble();
+//
+//        double areaOfCircle = Circle.getArea(radius);
+//        System.out.println("The Area of a Circle with a Radius of " + radius + " is: " + areaOfCircle);
 
-//        double areaOfCircle = (3.14 * radius * radius);
-        double areaOfCircle = Circle.getArea(radius);
 
-        System.out.println("The Area of a Circle of Radius " + radius + " is : " + areaOfCircle);
+        // Bonus Mission - Validation
+        System.out.println("Please enter a radius:");
+
+        if (input.hasNextDouble()) {
+            radius = input.nextDouble();
+
+            if (radius > 0) {
+                areaOfCircle = Circle.getArea(radius);
+                System.out.println("The Area of a Circle with a Radius of " + radius + " is: " + areaOfCircle);
+            } else {
+                System.out.println("The entered Radius should be a POSITIVE number and is NOT a valid input!");
+            }
+
+        } else {
+            System.out.println("The entered Radius is NOT a valid input!");
+        }
+
+        input.close();
     }
 }
