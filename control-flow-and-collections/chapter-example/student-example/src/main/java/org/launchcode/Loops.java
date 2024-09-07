a3da12540d4e47228943fda33a9f085077171b72
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-07 16:18:53 -0500

    CT completed - added While loop example

--- control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/Loops.java
+++ control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/Loops.java
@@ -3,14 +3,19 @@ package org.launchcode;
 public class Loops {
     public static void main(String[] args) {
 
-        // For Loop Examples - Using i++ increment operator
+        // For Loop Examples - Using i++ Increment operator
         System.out.println("~~~~~ For Loop Examples ~~~~~~");
+        System.out.println("Using Increment Operator");
 
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
 
-        // For Loop Examples - Using i-- increment operator
+
+        // For Loop Examples - Using i-- Decrement operator
+        System.out.println("");
+        System.out.println("Using Decrement Operator");
+
         for (int i = 10; i > 0; i--) {
             System.out.println(i);
         }
@@ -24,6 +29,7 @@ public class Loops {
         // For-Each Loop - iterates over any sequence/collection -such as Array
         int nums[] = {1, 1, 2, 3,5, 8, 13, 21};
 
+        // For EACH integer in Array 'nums' - Print each integer
         for (int i : nums) {
             System.out.println(i);
         }
@@ -32,8 +38,21 @@ public class Loops {
         System.out.println("");
         String msg = "Hello World";
 
+        // For EACH character in 'msg' - Convert to an Array of characters and Print each character
         for (char c : msg.toCharArray()) {
             System.out.println(c);
+        }
+
+
+
+        // While Loop examples
+        System.out.println("");
+        System.out.println("~~~~~~ While Loop Examples ~~~~~~");
+
+        int w = 0;
+        while (w < 3) {
+            System.out.println(w);
+            w++;
         }
 
 
