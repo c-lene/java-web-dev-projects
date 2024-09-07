3708c6676468d5215bf5a7efb5956e0fea215cba
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-07 18:30:16 -0500

    CT completed - ArrayList examples

--- control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/ArrayListPlanets.java
+++ control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/ArrayListPlanets.java
@@ -1,4 +1,27 @@
 package org.launchcode;
 
+import java.util.ArrayList;
+
 public class ArrayListPlanets {
+    public static void main(String [] args) {
+
+        // Declare & Initialize a new ArrayList called 'planets'
+        ArrayList<String> planets = new ArrayList<>();
+
+        // Using add() method to populate a collection of items to 'planets' ArrayList
+        planets.add("Mercury");
+        planets.add("Venus");
+        planets.add("Earth");
+        planets.add("Mars");
+        planets.add("Jupiter");
+        planets.add("Saturn");
+        planets.add("Uranus");
+        planets.add("Neptune");
+
+        // Declares a new String Array called 'planetsArray' with a size of 'planets' ArrayList
+        String[] planetsArray = new String[planets.size()];
+
+        // Returns 'planetsArray' Array containing the elements of the 'planets' Array
+        planets.toArray(planetsArray);
+    }
 }
