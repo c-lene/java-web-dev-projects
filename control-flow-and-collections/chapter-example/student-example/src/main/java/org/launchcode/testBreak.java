950c803c145dda156d16d1fd5f68fea4fc29f179
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-07 14:53:30 -0500

    CT completed - Break Statement example

--- control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/testBreak.java
+++ control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/testBreak.java
@@ -1,4 +1,16 @@
 package org.launchcode;
 
 public class testBreak {
+    public static void main(String[] args) {
+        int[] someInts = {1, 10, 2, 3, 5, 8, 10};
+        int searchTerm = 10;
+
+        for (int oneInt : someInts) {
+            System.out.println(oneInt);
+            if (oneInt == searchTerm) {
+                System.out.println("Found it!");
+                break;
+            }
+        }
+    }
 }
