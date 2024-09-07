98a8a3c702ef7658402ee47b610c02ce08c055f1
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-07 15:02:25 -0500

    CT completed - Break Statment example in Loops

--- control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/testBreak.java
+++ control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/testBreak.java
@@ -5,8 +5,11 @@ public class testBreak {
         int[] someInts = {1, 10, 2, 3, 5, 8, 10};
         int searchTerm = 10;
 
+        // Using For Loop to search each element in Array for 'someInt'
         for (int oneInt : someInts) {
             System.out.println(oneInt);
+
+            // Using IF statement - IF 'oneInt' is EQUAL TO 'searchTerm' THEN print out "Found it!" & TERMINATE Loop
             if (oneInt == searchTerm) {
                 System.out.println("Found it!");
                 break;
