ca704cf9864be4d456169f3c531c63cc7babe898
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-07 15:06:34 -0500

    CT completed - Continue Statments in Loops example

--- control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/testContinue.java
+++ control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/testContinue.java
@@ -1,4 +1,21 @@
 package org.launchcode;
 
 public class testContinue {
+    public static void main(String [] args) {
+        int[] someInts = {1, 10, 2, 3, 5, 8, 10};
+        int searchTerm = 10;
+
+        // Using For Loop to iterate through each element in Array of 'someInts'
+        for (int oneInt : someInts) {
+
+            // Using IF statement to search for 'searchTerm' & CONTINUE through the Loop
+            if (oneInt == searchTerm) {
+                System.out.println(oneInt + " - Found It!");
+                continue;
+            }
+
+            // Prints out EACH unmatched element in the loop as it iterates through the Array
+            System.out.println(oneInt + " - Not here!");
+        }
+    }
 }
