655cd1d4983c73fb2ad87e7ab9da00c3461e3d59
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-07 19:17:16 -0500

    CT completed - added comments to explain certain steps

--- control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/HashMapGradebook.java
+++ control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/HashMapGradebook.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 
 public class HashMapGradebook {
     public static void main(String[] args) {
+
+        // Declaring a HashMap 'students' with a String = key, Double = value
         HashMap<String, Double> students = new HashMap<>();
         Scanner input = new Scanner(System.in);
         String newStudent;
@@ -21,6 +23,8 @@ public class HashMapGradebook {
             if (!newStudent.equals("")) {
                 System.out.print("Grade: ");
                 Double newGrade = input.nextDouble();
+
+                // Using put() method to specify both key & value
                 students.put(newStudent, newGrade);
 
                 // Read in the newline before looping back
@@ -33,6 +37,7 @@ public class HashMapGradebook {
         System.out.println("\nClass roster:");
         double sum = 0.0;
 
+        // In the For-Each Loop - Uses Map.Entry to represent key/value pairs within HashMaps
         for (Map.Entry<String, Double> student : students.entrySet()) {
             System.out.println(student.getKey() + " (" + student.getValue() + ")");
             sum += student.getValue();
