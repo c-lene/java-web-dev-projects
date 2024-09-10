5ef207d2539b2a6cf6c697284ba68e99fb8b4394
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-09 21:53:49 -0500

    CT edit - removed unused variables & cleaned up formating

--- control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/CountingCharacters.java
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/CountingCharacters.java
@@ -8,8 +8,7 @@ public class CountingCharacters {
 
         // Declaring & Initializing varibales
         String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
-        Integer letterCount = 0;
-        Integer letterValue = 0;
+        int letterValue = 0;
 
         char[] charactersInString = quote.toCharArray();
 
