3df1af0956abd56b6a390ea738bb998928a523b2
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-11 22:51:26 -0500

    CT edited - created an instance of class HelloWorld to execute sayHello()

--- classes-part-one/Examples-CT/src/main/java/org/launchcode/HelloWorldRunner.java
+++ classes-part-one/Examples-CT/src/main/java/org/launchcode/HelloWorldRunner.java
@@ -1,4 +1,12 @@
 package org.launchcode;
 
 public class HelloWorldRunner {
+    public static void main(String[] args) {
+
+        // Created an Instance of the class HelloWorld
+        HelloWorld hello = new HelloWorld();
+
+        // Executes sayHello() 
+        hello.sayHello();
+    }
 }
