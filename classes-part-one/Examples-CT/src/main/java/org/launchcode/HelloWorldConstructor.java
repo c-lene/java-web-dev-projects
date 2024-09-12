bf912cb464b98f95a8469cd894a06138dbb52319
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-12 02:27:37 -0500

    CT edit - added Constuctors, Getter & Setters

--- classes-part-one/Examples-CT/src/main/java/org/launchcode/HelloWorldConstructor.java
+++ classes-part-one/Examples-CT/src/main/java/org/launchcode/HelloWorldConstructor.java
@@ -1,4 +1,18 @@
 package org.launchcode;
 
 public class HelloWorldConstructor {
+
+    private String message = "Hello World";
+
+    public HelloWorldConstructor(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String aMessage) {
+        message = aMessage;
+    }
 }
