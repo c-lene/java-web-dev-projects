7374b27ac272b429f297b6643988de99e2706b5e
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-12 01:15:55 -0500

    CT completed - found 2 errors in the Example

--- classes-part-one/Examples-CT/src/main/java/org/launchcode/Greeting.java
+++ classes-part-one/Examples-CT/src/main/java/org/launchcode/Greeting.java
@@ -1,4 +1,9 @@
 package org.launchcode;
 
 public class Greeting {
+    public String name = "Jess"
+
+    public void sayHello() {
+        System.out.println("Hello " + here.name + "!");
+    }
 }
