c56c9318229b67f2eb5dec9784d28597d38966fd
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-12 16:06:00 -0500

    CT edit - Added Method 2 Example for Using Multiple Constructors

--- classes-part-one/Examples-CT/src/main/java/org/launchcode/StudentConstructorOverloading.java
+++ classes-part-one/Examples-CT/src/main/java/org/launchcode/StudentConstructorOverloading.java
@@ -7,9 +7,26 @@ public class StudentConstructorOverloading {
     private int numberOfCredits;
     private double gpa;
 
-    // Using Multiple Constructors - Method 1
+//    // Using Multiple Constructors - Method 1
+//
+//    // Useful for creating Transfer Students with pre-exisiting credits & gpa (Non-Zero)
+//    public Student(String name, int studentId, int numberOfCredits, double gpa) {
+//        this.name = name;
+//        this.studentId = studentId;
+//        this.numberOfCredits = numberOfCredits;
+//        this.gpa = gpa;
+//    }
+//
+//    // Useful for creating New Students - Initializes credits & gpa to 0
+//    public Student(String name, int studentId) {
+//        this.name = name;
+//        this.studentId = studentId;
+//        this.numberOfCredits = 0;
+//        this.gpa = 0.0;
+//    }
 
-    // Useful for creating Transfer Students with pre-exisiting credits & gpa (Non-Zero)
+
+    // Using Multiple Constructors - Method 2
     public Student(String name, int studentId, int numberOfCredits, double gpa) {
         this.name = name;
         this.studentId = studentId;
@@ -17,13 +34,8 @@ public class StudentConstructorOverloading {
         this.gpa = gpa;
     }
 
-    // Useful for creating New Students - Initializes credits & gpa to 0
     public Student(String name, int studentId) {
-        this.name = name;
-        this.studentId = studentId;
-        this.numberOfCredits = 0;
-        this.gpa = 0.0;
+        this(name, studentId, 0, 0);
     }
-
 
 }
