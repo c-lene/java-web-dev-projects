b451227689314e051e334ac71444c88222ed84ca
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-12 01:31:58 -0500

    CT edited - Added Access Modifier Example - Omitting 'Public' Access Modifier example

--- classes-part-one/Examples-CT/src/main/java/org/launchcode/Greeting.java
+++ classes-part-one/Examples-CT/src/main/java/org/launchcode/Greeting.java
@@ -1,9 +1,17 @@
 package org.launchcode;
 
 public class Greeting {
-    public String name = "Jess"
+//    public String name = "Jess"
+//
+//    public void sayHello() {
+//        System.out.println("Hello " + here.name + "!");
+//    }
 
-    public void sayHello() {
-        System.out.println("Hello " + here.name + "!");
+
+    // Access Modifier Example - omiting "Public" Access Modifier gives 'message' field & sayHello() default access
+    String message = "Hello World";
+
+    void sayHello() {
+        System.out.println(message);
     }
 }
