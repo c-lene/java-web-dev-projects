cab233bd762efec0c6a2fdf77201226d1d2a4135
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-12 20:52:27 -0500

    CT edit - Added fields, constructors, & getter/setters for Menu

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -1,4 +1,21 @@
 package org.launchcode;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Menu {
+
+    // Declaring Fields
+    private ArrayList<MenuItem> menuItems = new ArrayList<>();
+    private LocalDate dateLastUpdated;
+
+
+    // Using DEFAULT Constructor
+
+
+    // Setter for 'menuItems'
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuItems;
+    }
 }
