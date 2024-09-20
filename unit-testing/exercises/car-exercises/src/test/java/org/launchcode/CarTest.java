4da62bfa2d858324cdd8c03effe4e7234db39992
Charlene Tran <charlenetran@Charlenes-MBP.attlocal.net>
2024-09-19 23:20:02 -0500

    CT completed - added code to complete TODO tasks

--- unit-testing/exercises/car-exercises/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/exercises/car-exercises/src/test/java/org/launchcode/CarTest.java
@@ -13,11 +13,15 @@ class CarTest {
         test_car = new Car("Toyota", "Prius", 10, 50);
     }
 
+
+
     //TODO: add emptyTest so we can configure our runtime environment
     @Test
     public void emptyTest() {
         assertEquals(10, 10, .001);
     }
+
+
 
     //TODO: constructor sets gasTankLevel properly
     @Test
@@ -25,7 +29,29 @@ class CarTest {
         assertEquals( 10, test_car.getGasTankLevel(),.001);
     }
 
+
+
     //TODO: gasTankLevel is accurate after driving within tank range
+    @Test
+    public void testGasTankAfterDriving() {
+        test_car.drive(50);
+        assertEquals(9, test_car.getGasTankLevel(), .001);
+    }
+
+
+
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+    @Test
+    public void testGasTankAfterExceedingTankRange() {
+        test_car.drive(500);
+        assertEquals(test_car.getGasTankLevel(), 0, .001);
+    }
+
+
+
     //TODO: can't have more gas than tank size, expect an exception
+    @Test
+    public void testGasOverfillException() {
+        assertThrows(IllegalArgumentException.class, () -> test_car.addGas(5), "Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+    }
 }
