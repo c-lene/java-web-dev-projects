b6183db7c83f5e1c3a7500af961c25aac61b00c3
Charlene Tran <charlenetran@Charlenes-MBP.attlocal.net>
2024-09-19 22:23:39 -0500

    CT edit - added code to test equals() method

--- classes-part-2/examples/src/main/java/org/launchcode/Pet.java
+++ classes-part-2/examples/src/main/java/org/launchcode/Pet.java
@@ -1,4 +1,46 @@
 package org.launchcode;
 
 public class Pet {
+    private String name;
+
+
+    // CONSTRUCTOR
+    Pet(String name) {
+        this.name = name;
+    }
+
+
+    // GETTERS
+    public String getName() {
+        return name;
+    }
+
+
+    public boolean equals(Object petToCheck) {
+
+        // Reference Check
+        if (petToCheck == this) {
+            return true;
+        }
+
+
+        // Null check
+        if (petToCheck == null) {
+            return false;
+        }
+
+
+        // Class check
+        if (petToCheck.getClass() != getClass()) {
+            return false;
+        }
+
+
+        // Cast
+        Pet thePet = (Pet) petToCheck;
+        return thePet.getName() == getName();
+    }
+
 }
+
+
