634b35f7f2ffb7f9a47231d5fe71be154f021184
Charlene Tran <charlenetran@Charlenes-MBP.attlocal.net>
2024-09-19 22:28:11 -0500

    CT edit - added code from book to complete example

--- classes-part-2/examples/src/main/java/org/launchcode/Temperature.java
+++ classes-part-2/examples/src/main/java/org/launchcode/Temperature.java
@@ -20,5 +20,24 @@ public class Temperature {
     }
 
     /* rest of the class... */
+    public static void main(String[] args) {
+
+        public Temperature temp = new Temperature();
+
+
+        // Use a static field the same way as a normal, non-static field
+        System.out.println(("Absolute zero in F is: " + absoluteZeroFahrenheit));
+
+        // We can also be more explicit - using "this" keyword does NOT work
+//        System.out.println("Absolute zero in F is: " + this.absoluteZeroFahrenheit);
+
+        // If the static field is public, we can do this
+        System.out.println("Absolute zero in F is: " + Temperature.absoluteZeroFahrenheit);
+
+        // OR if we have an object named "temp" of type Temperature
+        System.out.println("Absolute zero in F is: " + temp.absoluteZeroFahrenheit);
+
+
+    }
 
 }
