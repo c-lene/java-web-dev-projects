4d44523ddc3fa6dfbe3f1fe594e90ac81431d07a
Charlene Tran <charlenetran@Charlenes-MBP.attlocal.net>
2024-09-19 22:29:53 -0500

    CT edit - Incomplete - Added list of TODO

--- classes-part-2/studio/src/main/java/org/launchcode/Main.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Main.java
@@ -4,5 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+
+        // TODO: Create 5 menu items in different categories
+
+        // TODO: Print first item
+
+        // TODO: Create menu
+
+        // TODO: Add items to menu and print it
+
+        // TODO: Remove an item and print menu
+
+        // TODO: Test equals method
+
+
+
+
+        // BONUS MISSION
+
+        // TODO: Attempt to add a duplicate item, then print menu to confirm it wasn't added
+
+
     }
 }
