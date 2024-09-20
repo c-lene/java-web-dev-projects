bc1b2d06dc7bd106519db845494683f99762d6c4
Charlene Tran <charlenetran@Charlenes-MBP.attlocal.net>
2024-09-19 22:24:45 -0500

    CT edited - added code to execute the Pet class

--- classes-part-2/examples/src/main/java/org/launchcode/PetMain.java
+++ classes-part-2/examples/src/main/java/org/launchcode/PetMain.java
@@ -1,4 +1,17 @@
 package org.launchcode;
 
 public class PetMain {
+    public static void main(String[] args) {
+        String firstPet = "Fluffy";
+        Pet secondPet = new Pet("Fluffy");
+        Pet thirdPet = new Pet("Fluffy");
+
+//        System.out.println(firstPet == secondPet);
+        System.out.println(secondPet == thirdPet);
+        System.out.println(thirdPet.equals(secondPet));
+        System.out.println(thirdPet.getName().equals(firstPet));
+        System.out.println(thirdPet.equals(firstPet));
+
+
+    }
 }
