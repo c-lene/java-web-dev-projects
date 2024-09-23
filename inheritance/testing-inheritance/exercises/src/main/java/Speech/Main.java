b4c89cb9bf89db57a1b0214a0aa9c15de971b18b
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-23 03:25:10 -0500

    CT edit - Added example for Casting & Polymorphism

--- inheritance/testing-inheritance/exercises/src/main/java/Speech/Main.java
+++ inheritance/testing-inheritance/exercises/src/main/java/Speech/Main.java
@@ -1,4 +1,8 @@
 package Speech;
 
 public class Main {
+    public static void main(String[] args) {
+        Message hello = new Greeting("English", "Hello Coder!");
+        ((Greeting) hello).wave();
+    }
 }
