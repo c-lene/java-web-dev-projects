8e09b44ee1af202e39f0bfa3a2c0c106002b13dd
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-23 15:57:59 -0500

    CT edit - Added 3 test to test inherited methods & constructors from Cat

--- inheritance/testing-inheritance/exercises/src/test/java/CatTest.java
+++ inheritance/testing-inheritance/exercises/src/test/java/CatTest.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CatTest {
+    @Test
+    public void inheritsSuperInFirstConstructor() {
+        HouseCat keyboardCat = new HouseCat("Keyboard Cat",7);
+        assertEquals(7,keyboardCat.getWeight(), .001);
+    }
+
+    @Test
+    public void inheritsDefaultCatInSecondConstructor() {
+        HouseCat keyboardCat = new HouseCat("Keyboard Cat");
+        assertEquals(13, keyboardCat.getWeight(), .001);
+        assertEquals(13, keyboardCat.getWeight());
+
+    }
+
+    @Test
+    public void inheritsMethodsInCats() {
+        HouseCat keyboardCat = new HouseCat("Keyboard Cat");
+        keyboardCat.eat();
+        assertTrue(keyboardCat.isTired());
+    }
 }
