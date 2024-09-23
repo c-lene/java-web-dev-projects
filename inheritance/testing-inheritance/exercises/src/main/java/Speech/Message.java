c7697b6229a72bed39fd4537abdb2e27bb3b3a48
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-23 02:54:25 -0500

    CT edit - Added Fields, Constructor, & Getters

--- inheritance/testing-inheritance/exercises/src/main/java/Speech/Message.java
+++ inheritance/testing-inheritance/exercises/src/main/java/Speech/Message.java
@@ -1,4 +1,29 @@
+/* CT created to answer Check Your Understanding in "A Tale of Two Cats" section */
+
 package Speech;
 
 public class Message {
+    private boolean friendly = true;
+    private String language;
+    private String text;
+
+    /**** Constructor ****/
+    public Message(String aLanguage, String aText) {
+        language = aLanguage;
+        text = aText;
+    }
+
+
+    /**** Getters & Setters ****/
+    public boolean isFriendly() {
+        return friendly;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getText() {
+        return text;
+    }
 }
