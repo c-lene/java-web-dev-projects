25120d11a789b4f773e64c98b169e3f3acba5e7a
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-23 01:49:15 -0500

    CT edited - Added inherited methods from Cat class to apply to Housecat variable

--- inheritance/testing-inheritance/exercises/src/main/java/Main.java
+++ inheritance/testing-inheritance/exercises/src/main/java/Main.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+        HouseCat garfield = new HouseCat("Garfield", 12);
+
+        // Instance method eat() is inherited from Cat class
+        garfield.eat();
+
+
+        // isTired() is an inherited getter from Cat class - Prints "true"
+        System.out.println(garfield.isTired());
+    }
+
 }
