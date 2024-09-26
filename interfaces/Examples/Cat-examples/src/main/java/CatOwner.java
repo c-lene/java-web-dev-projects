ae608678eaff62484596cf5d248176c725a285c3
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-26 02:57:10 -0500

    CT edit - Added code to complete Example

--- interfaces/Examples/Cat-examples/src/main/java/CatOwner.java
+++ interfaces/Examples/Cat-examples/src/main/java/CatOwner.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class CatOwner {
+
+    /**** Fields ****/
+    private Cat pet;
+
+
+
+    /**** Constructors ****/
+    public CatOwner(Cat pet) {
+        this.pet = pet;
+    }
+
+
+
+    /**** Methods ****/
+
+    // Methods uses the field 'pet', which is a type 'Cat'
+    public void feedTheCat() {
+        pet.eat();
+    }
+
+
 }
