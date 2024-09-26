b3c46ac5821762b20d5b201f791f4406ef71e65d
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-26 02:55:55 -0500

    CT edited - Code class copied from Inheritance Cat example

--- interfaces/Examples/Cat-examples/src/main/java/Cat.java
+++ interfaces/Examples/Cat-examples/src/main/java/Cat.java
@@ -1,4 +1,76 @@
-package PACKAGE_NAME;
+/* Copied from Inheritance Chapter Examples */
 
 public class Cat {
+
+    private boolean tired = false;
+    private boolean hungry = false;
+    private double weight;
+
+    // The biological family for all cat species
+    private String family = "Felidae";
+
+
+    /**** Constructor ****/
+    public Cat (double aWeight) {
+        weight = aWeight;
+    }
+
+    // Adding additional Constructor in Cat
+    public Cat() {
+        weight = 13;
+    }
+
+
+    /**** Getters and Setters ****/
+
+    public boolean isTired() {
+        return tired;
+    }
+
+    public void setTired(boolean aTired) {
+        tired = aTired;
+    }
+
+    public boolean isHungry() {
+        return hungry;
+    }
+
+    public void setHungry(boolean aHungry) {
+        hungry = aHungry;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double aWeight) {
+        weight = aWeight;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    /**** Instance Methods ****/
+
+    // A cat is rested and hungry after it sleeps
+    public void sleep() {
+        tired = false;
+        hungry = true;
+    }
+
+    // Eating makes a cat not hungry
+    public void eat() {
+
+        // eating when not hungry makes a cat sleepy
+        if (!hungry) {
+            tired = true;
+        }
+
+        hungry = false;
+    }
+
+    public String noise () {
+        return "Meeeeeeooooowww!";
+    }
 }
