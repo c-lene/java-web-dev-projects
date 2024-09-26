2fb259d2cf8fd2c36a975bf3c1ab8de4755d33e0
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-26 02:58:04 -0500

    CT edit - Added code to run the classes

--- interfaces/Examples/Cat-examples/src/main/java/Main.java
+++ interfaces/Examples/Cat-examples/src/main/java/Main.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+
+        HouseCat suki = new HouseCat("Suki", 12);
+        CatOwner Annie = new CatOwner(suki);
+
+        Annie.feedTheCat();
+
+    }
+
 }
