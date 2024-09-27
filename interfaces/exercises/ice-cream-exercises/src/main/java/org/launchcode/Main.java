3c4b722344a195dcca93232fc4c40f4c35bc25ba
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-27 01:33:12 -0500

    CT completed - updated Main class to call ConeComparator to sort cones by cost

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -23,26 +23,43 @@ public class Main {
 
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
 
+        // STEP 1: Creates new 'ConeComparator' object
+        Comparator comparator1 = new ConeComparator();
+
+        // STEP 2: Calls the Sort method on 'cones' and pass the 'comparator1' object as the argument
+        cones.sort(comparator1);
+
+
+        // TIP: Instead of Declaring & Initializing comparator object (STEPS 1-2) - Both steps can be combined to a SINGLE statement
+//        flavors.sort(new ConeComparator());
+
+
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
 
-        // Flavor List ~~~~~~~~~~~~~~~~~
-        System.out.println("\nSORTED Flavors list: ");
+        /**** SORTED Flavors List by Name ****/
+        System.out.println("\nSORTED Flavors list by Name: ");
         System.out.println("");
 
         // Iterates through SORTED flavor list to print results
-        for (Flavor flavorName : flavors) {
+        for (Flavor flavor : flavors) {
 
             // Prints out the Overrided toString() format in Ingredients
 //            System.out.println(flavorName);\
 
-            // Prints out the list of flavor names
-            System.out.println(flavorName.getName());
+            // Prints out the list of flavor names sorted alphabetically by name
+            System.out.println(flavor.getName());
         }
 
 
+        /**** SORTED Cones List by Cost ****/
+        System.out.println("\nSORTED Coness list by Cost: ");
+        System.out.println("");
 
+        // Iterates through SORTED flavor list to print results
+        for (Cone cone : cones) {
 
-
-
+            // Prints out the list of cone names & cost sorted by cost
+            System.out.println(cone.getName() + ": " + "$" + cone.getCost());
+        }
     }
 }
