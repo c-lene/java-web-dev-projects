4205f1d7bf4b80e970632889bc95d8cd1b168d70
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-27 00:44:18 -0500

    CT edit - added a Comparator class to sort the 'flavors' ArrayList and print out the results

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -1,6 +1,7 @@
 package org.launchcode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +11,38 @@ public class Main {
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
 
+        // STEP 1: Creates new 'FlavorComparator' object
+        Comparator comparator = new FlavorComparator();
+
+        // STEP 2: Calls the Sort method on 'flavors' and pass the 'comparator' object as the argument
+        flavors.sort(comparator);
+
+        // TIP: Instead of Declaring & Initializing comparator object (STEPS 1-2) - Both steps can be combined to a SINGLE statement
+//        flavors.sort(new FlavorComparator());
+
+
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
+        // Flavor List ~~~~~~~~~~~~~~~~~
+        System.out.println("\nSORTED Flavors list: ");
+        System.out.println("");
+
+        // Iterates through SORTED flavor list to print results
+        for (Flavor flavorName : flavors) {
+
+            // Prints out the Overrided toString() format in Ingredients
+//            System.out.println(flavorName);\
+
+            // Prints out the list of flavor names
+            System.out.println(flavorName.getName());
+        }
+
+
+
+
+
+
     }
 }
