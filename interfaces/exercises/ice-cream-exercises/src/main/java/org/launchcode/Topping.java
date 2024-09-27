d1a95ba55607ee7e0f31496dd7210f19b3fbec96
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-27 02:37:22 -0500

    CT completed - BONUS MISSION 2 - completed creating Topping class

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Topping.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Topping.java
@@ -1,4 +1,11 @@
 package org.launchcode;
 
-public class Topping {
+import java.util.ArrayList;
+
+// BONUS MISSION #2: Create Topping class that extends Ingredient class
+public class Topping extends Ingredient{
+
+    public Topping(String aName, double aCost, ArrayList<String> someAllergens) {
+        super(aName, aCost, someAllergens);
+    }
 }
