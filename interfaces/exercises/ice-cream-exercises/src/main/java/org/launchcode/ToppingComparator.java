8755f3071ad74e53aeb44e8cc4b3b4f0b089435c
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-09-27 03:11:08 -0500

    CT completed - BONUS MISSION 2 - updated to Sort Toppings based on Number of Allergens from Lowest to Highest

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/ToppingComparator.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/ToppingComparator.java
@@ -1,4 +1,22 @@
 package org.launchcode;
 
-public class ToppingComparator {
+import java.util.Comparator;
+
+// BONUS MISSION 2: Sort Flavor objects by the NUMBER of Allergens from LOWEST to HIGHEST
+public class ToppingComparator implements Comparator<Topping> {
+    @Override
+    public int compare(Topping topping1, Topping topping2) {
+        if (topping1.getAllergens().size() - topping2.getAllergens().size() < 0) {
+            // Returns NEGATIVE if '# of Allergens in flavor1' < '# of Allergens in flavor2'
+            return -1;
+
+        } else if (topping1.getAllergens().size() - topping2.getAllergens().size() > 0) {
+            // Returns POSITIVE if '# of Allergens in flavor1' > '# of Allergens in flavor2'
+            return 1;
+
+        } else {
+            // Return "0" indicates No Sorting if '# of Allergens in flavor1' == '# of Allergens in flavor2'
+            return 0;
+        }
+    }
 }
