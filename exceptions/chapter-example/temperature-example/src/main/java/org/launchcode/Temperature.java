62a0871626ed1ff9cb5371480cd1cd4483061b9a
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-10-02 23:55:32 -0500

    CT edit - Added Try/Catch block using TemperatureExaception

--- exceptions/chapter-example/temperature-example/src/main/java/org/launchcode/Temperature.java
+++ exceptions/chapter-example/temperature-example/src/main/java/org/launchcode/Temperature.java
@@ -12,6 +12,16 @@ public class Temperature {
         double absoluteZeroFahrenheit = -459.67;
 
         if (aFahrenheit < absoluteZeroFahrenheit) {
+
+            // Using Try/Catch block to TRY throwing the Exception, if CATCH error (e) ==> Print the stack trace
+            try {
+                throw new TemperatureException("That temperature is TOO LOW!");
+            } catch (TemperatureException e) {
+                e.printStackTrace();
+            }
+
+
+
             System.out.println("Value is below absolute zero");
         }
 
