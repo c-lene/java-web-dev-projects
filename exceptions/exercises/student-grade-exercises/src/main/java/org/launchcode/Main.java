8a329464595c8ebc3acfee5aa8062efb517639f7
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-10-03 00:25:24 -0500

    CT edit - Added an Try/Catch block if y < 0 and to print out result of x/y

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+        Divide(3,0);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -15,9 +16,21 @@ public class Main {
         // Test out your CheckFileExtension() function!
     }
 
-    public static void Divide(int x, int y)
-    {
+    // Changed "void" to "int"
+    public static void Divide(int x, int y) {
         // Write code here!
+        int result;
+
+        if (y <= 0) {
+            try {
+                throw new ArithmeticException("The value of 'y' is <= 0.");
+            } catch (ArithmeticException e) {
+                System.out.println("The value of 'y' must be > 0.");
+            }
+        }
+
+        result = x/y;
+        System.out.println(result);
     }
 
     public static int CheckFileExtension(String fileName)
